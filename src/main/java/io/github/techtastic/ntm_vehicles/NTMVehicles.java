3480c6dce7f9cdabf5f91a2d6f55e5f5304eda40
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-10-12 16:52:53 -0500

    Added a gamerule for blast radius

--- src/main/java/io/github/techtastic/ntm_vehicles/NTMVehicles.java
+++ src/main/java/io/github/techtastic/ntm_vehicles/NTMVehicles.java
@@ -5,16 +5,20 @@ import com.hbm.entity.logic.EntityNukeExplosionMK5;
 import io.github.techtastic.ntm_vehicles.ntm_vehicles.Tags;
 import mcinterface1122.WrapperWorld;
 import minecrafttransportsimulator.entities.instances.EntityBullet;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
 import java.util.Map;
 
-@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = "required-after:mts@[22.18.0,);required-after:hbm;required-after:mixinbooter", acceptableRemoteVersions = "*")
 public class NTMVehicles {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
@@ -30,23 +34,23 @@ public class NTMVehicles {
             World actualWorld = null;
             try {
                 Class<WrapperWorld> clazz = WrapperWorld.class;
-                Field field = clazz.getDeclaredField("worldWrappers");
+                Field field = clazz.getDeclaredField("world");
                 field.setAccessible(true);
-                Map<World, WrapperWorld> wrappers = (Map<World, WrapperWorld>) field.get(world);
-                for (Map.Entry<World, WrapperWorld> entry : wrappers.entrySet()) {
-                    if (entry.getValue() == world) {
-                        actualWorld = entry.getKey();
-                        break;
-                    }
-                }
+                actualWorld = (World) field.get(world);
             } catch (Exception ignored) {}
 
             if (actualWorld == null)
                 return;
 
-            actualWorld.spawnEntity(EntityNukeExplosionMK5.statFac(actualWorld, 120, pos.x, pos.y, pos.z));
-            EntityNukeTorex.statFac(actualWorld, pos.x, pos.y, pos.z, 120);
+            actualWorld.spawnEntity(EntityNukeExplosionMK5.statFac(actualWorld, actualWorld.getGameRules().getInt("ivNukeBlastRadius"), pos.x, pos.y, pos.z));
+            EntityNukeTorex.statFac(actualWorld, pos.x, pos.y, pos.z, actualWorld.getGameRules().getInt("ivNukeBlastRadius"));
         });
+    }
+
+    @Mod.EventHandler
+    public void worldInit(WorldEvent event) {
+        if (!event.getWorld().getGameRules().hasRule("ivNukeBlastRadius"))
+            event.getWorld().getGameRules().addGameRule("ivNukeBlastRadius", "120", GameRules.ValueType.NUMERICAL_VALUE);
     }
 
 }
