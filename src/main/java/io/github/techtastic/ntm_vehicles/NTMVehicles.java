d395fbe0a4e1ff4b885a0c9d0b1bd43acef36ad2
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-10-14 08:01:35 -0500

    Added Chloride and Made Nuke depend on `blastStrength`

--- src/main/java/io/github/techtastic/ntm_vehicles/NTMVehicles.java
+++ src/main/java/io/github/techtastic/ntm_vehicles/NTMVehicles.java
@@ -2,7 +2,7 @@ package io.github.techtastic.ntm_vehicles;
 
 import com.hbm.entity.effect.EntityNukeTorex;
 import com.hbm.entity.logic.EntityNukeExplosionMK5;
-import io.github.techtastic.ntm_vehicles.ducks.INuclear;
+import com.hbm.explosion.ExplosionChaos;
 import io.github.techtastic.ntm_vehicles.ntm_vehicles.Tags;
 import mcinterface1122.WrapperWorld;
 import minecrafttransportsimulator.entities.instances.EntityBullet;
@@ -28,20 +28,42 @@ public class NTMVehicles {
     public void preInit(FMLPreInitializationEvent event) {
         EntityBullet.registerCustomHitFunction("ntm_vehicles:nuke", (world, pos, side, type, bullet) -> {
             World actualWorld = null;
-            INuclear nuke = null;
+            //INuclear nuke = null;
             try {
                 Class<WrapperWorld> clazz = WrapperWorld.class;
                 Field field = clazz.getDeclaredField("world");
                 field.setAccessible(true);
                 actualWorld = (World) field.get(world);
-                nuke = (INuclear) bullet.definition;
+                //nuke = (INuclear) bullet.definition.bullet;
             } catch (Exception ignored) {}
 
-            if (actualWorld == null || nuke == null)
+            if (actualWorld == null)
                 return;
 
-            actualWorld.spawnEntity(EntityNukeExplosionMK5.statFac(actualWorld, nuke.getNuclearBlastRadius(), pos.x, pos.y, pos.z));
-            EntityNukeTorex.statFac(actualWorld, pos.x, pos.y, pos.z, nuke.getNuclearBlastRadius());
+            // y = ((x/0.28)^3)/1000
+            // 1000y = (x/0.28)^3
+            // (1000y)^1/3 = x/0.28
+            // 0.28 * ((1000y)^1/3) = x
+
+            //int blastRadius = (int) (Math.pow(bullet.definition.bullet.blastStrength * 1000, 1d/3) * 0.28);
+
+            actualWorld.spawnEntity(EntityNukeExplosionMK5.statFac(actualWorld, (int) bullet.definition.bullet.blastStrength, pos.x, pos.y, pos.z));
+            EntityNukeTorex.statFac(actualWorld, pos.x, pos.y, pos.z, (int) bullet.definition.bullet.blastStrength);
+        });
+
+        EntityBullet.registerCustomHitFunction("nym_vehicles:chlorine", (world, pos, side, type, bullet) -> {
+            World actualWorld = null;
+            try {
+                Class<WrapperWorld> clazz = WrapperWorld.class;
+                Field field = clazz.getDeclaredField("world");
+                field.setAccessible(true);
+                actualWorld = (World) field.get(world);
+            } catch (Exception ignored) {}
+
+            if (actualWorld == null)
+                return;
+
+            ExplosionChaos.spawnChlorine(actualWorld, pos.x, pos.y, pos.z, 50, 1.25, 0);
         });
     }
 }
--- src/main/java/io/github/techtastic/ntm_vehicles/ducks/INuclear.java
+++
@@ -1,5 +0,0 @@
-package io.github.techtastic.ntm_vehicles.ducks;
-
-public interface INuclear {
-    int getNuclearBlastRadius();
-}
