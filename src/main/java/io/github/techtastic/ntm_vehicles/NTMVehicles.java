c492165b14103b17690aa3007a37370e353d950e
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-10-24 11:04:23 -0500

    Fixed dependency issue

--- src/main/java/io/github/techtastic/ntm_vehicles/NTMVehicles.java
+++ src/main/java/io/github/techtastic/ntm_vehicles/NTMVehicles.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = "required-after:mts@[22.18.0,);required-after:hbm;required-after:mixinbooter", acceptableRemoteVersions = "*")
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = "required-after:mts;required-after:hbm;required-after:mixinbooter", acceptableRemoteVersions = "*")
 public class NTMVehicles {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
